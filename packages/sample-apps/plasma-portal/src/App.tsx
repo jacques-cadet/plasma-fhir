2c3316cb3302c128ccc2496d265fd3e449e4e276
Eric <ericmorgan1@gmail.com>
2022-03-14 21:59:49 -0500

    Add footer; favicon; update list item

--- packages/sample-apps/plasma-portal/src/App.tsx
+++ packages/sample-apps/plasma-portal/src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import { FHIRClientContextWrapper } from "./plasma-fhir/FHIRClient";
-import { Navbar } from "./components";
+import { Navbar, Footer } from "./components";
 import { TestScreen, LandingScreen, LaunchScreen } from "./screens";
 import { PatientScreen, EncountersScreen, AllergiesScreen, FamilyHistoryScreen, ConditionsScreen, ImmunizationsScreen, LabsScreen, VitalsScreen } from "./screens";
 import './App.scss';
@@ -26,32 +26,35 @@ function WithoutNavbar() {
 
 export default function App() {
   return (
-    <div>
-      <BrowserRouter basename={routerBasename}>
-        <Routes>
-          
-          {/* Patient-Facing App with Landing Page */}
-          <Route element={<WithoutNavbar />}>
-            <Route path="/" element={<LandingScreen />} />
-            <Route path="/launch" element={<LaunchScreen />} />
-          </Route>
+    <div className="flex flex-col h-screen">
+      <div className="mb-auto">
+        <BrowserRouter basename={routerBasename}>
+          <Routes>
+            
+            {/* Patient-Facing App with Landing Page */}
+            <Route element={<WithoutNavbar />}>
+              <Route path="/" element={<LandingScreen />} />
+              <Route path="/launch" element={<LaunchScreen />} />
+            </Route>
 
-          {/* Pages */}
-          <Route element={<WithNavbar />}>
-            <Route path="/app" element={<FHIRClientContextWrapper><TestScreen /></FHIRClientContextWrapper>} />
-            <Route path="/patient" element={<FHIRClientContextWrapper><PatientScreen /></FHIRClientContextWrapper>} />
-            <Route path="/encounters" element={<FHIRClientContextWrapper><EncountersScreen /></FHIRClientContextWrapper>} />
-            <Route path="/allergies" element={<FHIRClientContextWrapper><AllergiesScreen /></FHIRClientContextWrapper>} />
-            <Route path="/familyHistory" element={<FHIRClientContextWrapper><FamilyHistoryScreen /></FHIRClientContextWrapper>} />
-            <Route path="/conditions" element={<FHIRClientContextWrapper><ConditionsScreen /></FHIRClientContextWrapper>} />
-            <Route path="/immunizations" element={<FHIRClientContextWrapper><ImmunizationsScreen /></FHIRClientContextWrapper>} />
-            <Route path="/labs" element={<FHIRClientContextWrapper><LabsScreen /></FHIRClientContextWrapper>} />
-            <Route path="/vitals" element={<FHIRClientContextWrapper><VitalsScreen /></FHIRClientContextWrapper>} />
-            <Route path="/test" element={<FHIRClientContextWrapper><TestScreen /></FHIRClientContextWrapper>} />
-          </Route>
+            {/* Pages */}
+            <Route element={<WithNavbar />}>
+              <Route path="/app" element={<FHIRClientContextWrapper><TestScreen /></FHIRClientContextWrapper>} />
+              <Route path="/patient" element={<FHIRClientContextWrapper><PatientScreen /></FHIRClientContextWrapper>} />
+              <Route path="/encounters" element={<FHIRClientContextWrapper><EncountersScreen /></FHIRClientContextWrapper>} />
+              <Route path="/allergies" element={<FHIRClientContextWrapper><AllergiesScreen /></FHIRClientContextWrapper>} />
+              <Route path="/familyHistory" element={<FHIRClientContextWrapper><FamilyHistoryScreen /></FHIRClientContextWrapper>} />
+              <Route path="/conditions" element={<FHIRClientContextWrapper><ConditionsScreen /></FHIRClientContextWrapper>} />
+              <Route path="/immunizations" element={<FHIRClientContextWrapper><ImmunizationsScreen /></FHIRClientContextWrapper>} />
+              <Route path="/labs" element={<FHIRClientContextWrapper><LabsScreen /></FHIRClientContextWrapper>} />
+              <Route path="/vitals" element={<FHIRClientContextWrapper><VitalsScreen /></FHIRClientContextWrapper>} />
+              <Route path="/test" element={<FHIRClientContextWrapper><TestScreen /></FHIRClientContextWrapper>} />
+            </Route>
 
-        </Routes>
-      </BrowserRouter>
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
     </div>
   );
 }
--- packages/sample-apps/plasma-portal/src/components/HealthSystemSearch/EndPointListItem.tsx
+++ packages/sample-apps/plasma-portal/src/components/HealthSystemSearch/EndPointListItem.tsx
@@ -9,11 +9,11 @@ export interface IEndPointListItemProps {
 
 export default function EndPointListItem(props: IEndPointListItemProps) {
     return (
-        <div className="p-3 text-left hover:bg-indigo-300">
-            <Link to="/launch" state={{ authParams: { ...props.authParams, iss: props.endpoint.address } }} className="text-center">
+        <Link to="/launch" state={{ authParams: { ...props.authParams, iss: props.endpoint.address } }} className="text-center">
+            <div className="p-3 text-left hover:bg-indigo-300">            
                 <span>{props.endpoint.name}</span><br />
                 <span className="text-sm">{props.endpoint.address}</span>
-            </Link>
-        </div>
+            </div>
+        </Link>
     )
 }
