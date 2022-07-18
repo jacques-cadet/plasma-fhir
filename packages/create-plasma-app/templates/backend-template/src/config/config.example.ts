15c5e65c8693fba37613d9a84fa62b21cfa2c9f2
Eric Morgan <ericmorgan1@gmail.com>
2022-07-17 23:44:29 -0500

    Bug in endpoints

--- packages/create-plasma-app/templates/backend-template/src/config/config.example.ts
+++ packages/create-plasma-app/templates/backend-template/src/config/config.example.ts
@@ -1,7 +1,7 @@
 const config = {
     // Epic (Backend-Context, Sandbox)
     "EPIC_BACKEND_SANDBOX": { 
-        "tokenUrl":         "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4/token",
+        "tokenUrl":         "https://fhir.epic.com/interconnect-fhir-oauth/oauth2/token",
         "iss":              "https://fhir.epic.com/interconnect-fhir-oauth/api/FHIR/R4",
         "clientId":         "{EPIC NON-PRODUCTION CLIENT ID FOR BACKEND APP}",
     },
